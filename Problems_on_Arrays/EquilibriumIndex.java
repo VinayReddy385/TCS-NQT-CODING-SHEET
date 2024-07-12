bb6698d9f506e47f510e51b0d7021ef0b2a13a7e
VinayReddy385 <133024003+VinayReddy385@users.noreply.github.com>
2024-07-12 22:53:49 +0530

    Update EquilibriumIndex.java

--- Problems_on_Arrays/EquilibriumIndex.java
+++ Problems_on_Arrays/EquilibriumIndex.java
@@ -4,48 +4,25 @@ import java.util.Scanner;
 
 public class EquilibriumIndex {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of elements:");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        int[] leftSumSoFar = new int[n];
-        int[] rightSumSoFar = new int[n];
-
-        // Calculate the left sum at each index
-        int leftSum = 0;
-        for (int i = 0; i < n; i++) {
-            leftSum += arr[i];
-            leftSumSoFar[i] = leftSum;
-        }
-
-        // Calculate the right sum at each index
-        int rightSum = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            rightSum += arr[i];
-            rightSumSoFar[i] = rightSum;
-        }
-
-        // Find the equilibrium index
-        int equilibriumIndex = -1;
-        for (int i = 0; i < n; i++) {
-            if (leftSumSoFar[i] == rightSumSoFar[i]) {
-                equilibriumIndex = i;
-                break;
-            }
-        }
-
-        // Print the result
-        if (equilibriumIndex != -1) {
-            System.out.println("Equilibrium index: " + equilibriumIndex);
-        } else {
-            System.out.println("No equilibrium index found.");
-        }
-
-        sc.close();
-    }
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int []arr=new int[n];
+	    for(int i=0;i<n;i++){
+	        arr[i]=sc.nextInt();
+	    }
+	    for(int j=1;j<n-1;j++){
+	        int rsum=arr[0];
+	        for(int r=1;r<j;r++){
+	            rsum+=arr[r];
+	        }
+	        int lsum=0;
+	        for(int l=j+1;l<n;l++){
+	            lsum+=arr[l];
+	        }
+	        if(lsum==rsum){
+	            System.out.println(j);
+	        }
+	    }
+	}
+       
 }
