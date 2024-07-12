b494900553272d8e80a9eba93c34816708f074cd
VinayReddy385 <133024003+VinayReddy385@users.noreply.github.com>
2024-07-12 15:35:22 +0530

    Update NumberToWords.java
    problem solved

--- Problems_On_Number_System/NumberToWords.java
+++ Problems_On_Number_System/NumberToWords.java
@@ -1,5 +1,11 @@
 package Problems_On_Number_System;
-
+import java.util.*;
 public class NumberToWords {
-    
+    public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    long num=sc.nextLong();
+	    String str=String.valueOf(num);
+	    System.out.println(str);
+	    
+	} 
 }
